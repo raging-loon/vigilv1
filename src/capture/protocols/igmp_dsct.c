fcb4dea0e6eecb1114ac8520d24364851972812c
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-11-30 15:45:54 +0000

    fix igmp

--- src/capture/protocols/igmp_dsct.c
+++ src/capture/protocols/igmp_dsct.c
@@ -8,7 +8,7 @@
 void ip4_igmp_decode(const unsigned char * pkt, const char * src_ip, const char * dest_ip){
   struct main_igmp * type_decode = (struct main_igmp *)(pkt + ETH_HDR_SZ + sizeof(struct iphdr));
   printf("IPv4 IGMP %s -> %s [", src_ip ,dest_ip);
-  switch(main_igmp->type){
+  switch(type_decode->type){
     case 0x11:{
       printf(" Membership Query ]\n");
       break;
@@ -17,12 +17,12 @@ void ip4_igmp_decode(const unsigned char * pkt, const char * src_ip, const char 
       struct igmp_member_report * igmp_mem_rep = (struct igmp_member_report *)(pkt + ETH_HDR_SZ + sizeof(struct iphdr));
 
       char multicast_ip[32];
-      strcpy(multicast_ip,ipv4_ntoa(igmp_mem_rep->multicast_address));
+      strcpy(multicast_ip,ipv4_ntoa((uint32_t*)igmp_mem_rep->multicast_address));
       printf(" Membership Report / Join group %s for any sources ]\n",multicast_ip);
       break;
     } 
     default:
-      printf("Unknown IGMP Type=%d ]\n",main_igmp->type);
+      printf("Unknown IGMP Type=%d ]\n",type_decode->type);
       break;
   } 
 }
