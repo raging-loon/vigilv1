927e0a1428cd1392528c2305a1f52f0a90d8d24b
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-01 17:18:06 +0000

    s

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -7,11 +7,12 @@
 #include "../../globals.h"
 #include <stdlib.h>
 void add_ip_addr_or_inc_counter(const char * ip_addr){
-  printf("[DEBUG] aiaoic = %s\n",ip_addr);
   pthread_t pthrd;
   aiaoic_args * args = malloc(sizeof(aiaoic_args));
   // args->__ip_addr = (char *)ip_addr;
   strcpy(args->__ip_addr,ip_addr);
+  printf("[DEBUG] aiaoic = %s\n",ip_addr);
+
   pthread_create(&pthrd, NULL, &verify_ip_addr, &args);
 
 }
@@ -21,6 +22,8 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
 static void* verify_ip_addr(void * args){
   // const char * ip_addr = ((struct aiaoic_args*)&args)->__ip_addr;
   aiaoic_args * temp_info = args;
+  printf("[DEBUG] aiaoic_args = %s\n",temp_info->__ip_addr);
+
   for(int i = 0; i <= ip_addr_stat_counter_len; i++){
     if(strcmp(temp_info->__ip_addr , ip_stats[ip_addr_stat_counter_len].ip_addr) == 0){
       ip_stats[i].count++;
