2376ae471ea2cf91e7065d2fa27df18bb6a68174
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-01 16:08:30 +0000

    j

--- src/statistics/ip_addr_stat.h
+++ src/statistics/ip_addr_stat.h
@@ -8,7 +8,7 @@ struct ip_addr_counter{
 void add_ip_addr_or_inc_counter(const char *);
 static void *verify_ip_addr(void *);
 typedef struct {
-  const char * __ip_addr;
+  char * __ip_addr;
 } aiaoic_args;
 
 
