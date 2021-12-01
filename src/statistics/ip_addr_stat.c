c24e436b3e54514cb96aaf544790c2b5e9ac64ca
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-01 17:49:24 +0000

    j

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -10,7 +10,7 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
   char cpy_ip_addr[24];
   pthread_t pthrd;
   aiaoic_args * args = (aiaoic_args*)malloc(sizeof(aiaoic_args));
-  strncpy(cpy_ip_addr,ip_addr,sizeof(ip_addr));
+  strncpy(cpy_ip_addr,ip_addr,sizeof(cpy_ip_addr));
   args->__ip_addr = (char *)&cpy_ip_addr;
 
   printf("[DEBUG] aiaoic = %s\n",args->__ip_addr);
