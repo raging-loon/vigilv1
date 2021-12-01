33afc31fb0fbf9b2cbb3130b95d3fc3381abbfc6
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-01 15:55:52 +0000

    globals.h

--- globals.h
+++ globals.h
@@ -2,7 +2,7 @@
 #define GLOBALS_H
 #include "src/statistics/ip_addr_stat.h"
 
-extern struct ip_addr_counter * ip_stats;
+extern struct ip_addr_counter ip_stats[256];
 extern int ip_addr_stat_counter_len;
 extern char ip_addr[32];
 
