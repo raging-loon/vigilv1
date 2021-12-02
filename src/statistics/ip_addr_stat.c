972228b0e063fe737fe1d10af222cc61b7ccf439
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-02 15:18:06 +0000

    s

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -18,7 +18,7 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
   printf("[DEBUG] aiaoic = %s\n",args->__ip_addr);
 
   pthread_create(&pthrd, NULL, &verify_ip_addr, (aiaoic_args *)&args);
-  pthread_join(&pthrd, NULL);
+  pthread_join(pthrd, NULL);
 
 }
 
