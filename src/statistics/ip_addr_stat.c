fd790f9573665e9acb3f47615d4d8acff7f3411b
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-02 17:21:14 +0000

    s

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -20,7 +20,7 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
   // printf("[DEBUG] aiaoic = %p\n",args->__ip_addr);
   printf("[DEBUG] aiaoic = %s\n",args->__ip_addr);
 
-  pthread_create(&pthrd, NULL, &verify_ip_addr, (void *)(aiaoic_args *)&args);
+  pthread_create(&pthrd, NULL, &verify_ip_addr, (void *)(aiaoic_args *)&args->__ip_addr);
   // pthread_join(pthrd, NULL);
 
 }
@@ -28,30 +28,22 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
 
 
 static void* verify_ip_addr(void * args){
-  // const char * ip_addr = ((aiaoic_args*)&args)->__ip_addr;
-  // printf("[DEBUG] ip_addr = %s\n",ip_addr);
-  aiaoic_args * temp_info = args;//(aiaoic_args*)malloc(sizeof(aiaoic_args));
-  // temp_info = args;
-
+  // aiaoic_args * temp_info = args;//(aiaoic_args*)malloc(sizeof(aiaoic_args));
+  const char * ip_addr = args;
   printf("[DEBUG] aiaoic_args = %s\n",temp_info->__ip_addr);
-  // printf("[DEBUG] aiaoic_args = %p\n",temp_info->__ip_addr);
   for(int i = 0; i <= ip_addr_stat_counter_len; i++){
-    if(strcmp(temp_info->__ip_addr , ip_stats[ip_addr_stat_counter_len].ip_addr) == 0){
+    if(strcmp(ip_addr , ip_stats[ip_addr_stat_counter_len].ip_addr) == 0){
       ip_stats[i].count++;
-      // free(temp_info);
       goto end; 
     }
   }
   struct ip_addr_counter * temp_stat = (struct ip_addr_counter*)malloc(sizeof(struct ip_addr_counter));
   ip_stats[++ip_addr_stat_counter_len] = (struct ip_addr_counter)*temp_stat;
-  memset(temp_stat->ip_addr,0,sizeof(temp_info->__ip_addr));
-  strcpy(ip_stats[ip_addr_stat_counter_len].ip_addr,temp_info->__ip_addr);
+  memset(temp_stat->ip_addr,0,sizeof(ip_addr));
+  strcpy(ip_stats[ip_addr_stat_counter_len].ip_addr,ip_addr);
   ip_stats[ip_addr_stat_counter_len].count++;
   // printf("[DEBUG] temp_info->__ip_addr = %s ip_stats[ip_addr_stat_counter_len].ip_addr = %s\n",temp_info->__ip_addr,ip_stats[ip_addr_stat_counter_len].ip_addr);
-  // free(temp_info);
+  
 end:;
-  // if(temp_info->__ip_addr)
-    // free(temp_info->__ip_addr);
-  // if(temp_info)
-
+  
 }
