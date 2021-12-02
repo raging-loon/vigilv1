d99489694f1dd08e5fd6075aac432cad0ca61298
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-02 15:26:18 +0000

    n

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -41,7 +41,7 @@ static void* verify_ip_addr(void * args){
   }
   struct ip_addr_counter * temp_stat = (struct ip_addr_counter*)malloc(sizeof(struct ip_addr_counter));
   ip_stats[++ip_addr_stat_counter_len] = (struct ip_addr_counter)*temp_stat;
-  memset(temp_stat->__ip_addr,0,sizeof(temp_info->__ip_addr));
+  memset(temp_stat->ip_addr,0,sizeof(temp_info->__ip_addr));
   strcpy(ip_stats[ip_addr_stat_counter_len].ip_addr,temp_info->__ip_addr);
   ip_stats[ip_addr_stat_counter_len].count++;
   // printf("[DEBUG] temp_info->__ip_addr = %s ip_stats[ip_addr_stat_counter_len].ip_addr = %s\n",temp_info->__ip_addr,ip_stats[ip_addr_stat_counter_len].ip_addr);
