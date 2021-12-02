e03834c853d17ab14116d5b1bcf502a71d8af456
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-02 15:33:54 +0000

    a

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -9,11 +9,12 @@
 void add_ip_addr_or_inc_counter(const char * ip_addr){
   char cpy_ip_addr[24];
   memset(&cpy_ip_addr,0,sizeof(cpy_ip_addr));
-  cpy_ip_addr = "";
+  
   pthread_t pthrd;
   aiaoic_args * args = (aiaoic_args*)malloc(sizeof(aiaoic_args));
   // args->__ip_addr = (char *)malloc(sizeof(cpy_ip_addr));
   memset(args->__ip_addr,0,sizeof(args->__ip_addr));
+  args->-__ip_addr = "";
   strncpy(cpy_ip_addr,ip_addr,strlen(ip_addr));
   printf("[DEBUG] cpy_ip_addr = %s\n",cpy_ip_addr);
   // int ip_addr_sz = strlen(ip_addr);
