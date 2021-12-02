c91afc48c2affbb75cb7cc353b806bc2a627370e
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-02 17:25:43 +0000

    s

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -21,7 +21,7 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
   printf("[DEBUG] aiaoic = %s\n",args->__ip_addr);
 
   pthread_create(&pthrd, NULL, &verify_ip_addr, (void *)&args->__ip_addr);
-  // pthread_join(pthrd, NULL);
+  pthread_join(pthrd, NULL);
 
 }
 
@@ -32,7 +32,8 @@ static void* verify_ip_addr(void * args){
   const char * ip_addr = args;
   // printf("[DEBUG] aiaoic_args = %s\n",temp_info->__ip_addr);
   for(int i = 0; i <= ip_addr_stat_counter_len; i++){
-    if(strcmp(ip_addr , ip_stats[ip_addr_stat_counter_len].ip_addr) == 0){
+    if(strcmp(ip_addr , ip_stats[i].ip_addr) == 0){
+      // printf("Found match: %s == %s\n",ip_addr,ip_stats[ip_addr_stat_counter_len].);
       ip_stats[i].count++;
       goto end; 
     }
