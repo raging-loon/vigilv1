802b71795424ca5e0178e1753f5c180679a0f42b
mikhaildmitrov <85407521+raging-loon@users.noreply.github.com>
2021-12-02 17:19:22 +0000

    n

--- src/statistics/ip_addr_stat.c
+++ src/statistics/ip_addr_stat.c
@@ -18,7 +18,7 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
 
   memcpy((char*)&args->__ip_addr,(char*)&cpy_ip_addr,sizeof(cpy_ip_addr));
   // printf("[DEBUG] aiaoic = %p\n",args->__ip_addr);
-  // printf("[DEBUG] aiaoic = %s\n",args->__ip_addr);
+  printf("[DEBUG] aiaoic = %s\n",args->__ip_addr);
 
   pthread_create(&pthrd, NULL, &verify_ip_addr, (void *)(aiaoic_args *)&args);
   pthread_join(pthrd, NULL);
@@ -28,8 +28,8 @@ void add_ip_addr_or_inc_counter(const char * ip_addr){
 
 
 static void* verify_ip_addr(void * args){
-  const char * ip_addr = ((aiaoic_args*)&args)->__ip_addr;
-  printf("[DEBUG] ip_addr = %s\n",ip_addr);
+  // const char * ip_addr = ((aiaoic_args*)&args)->__ip_addr;
+  // printf("[DEBUG] ip_addr = %s\n",ip_addr);
   aiaoic_args * temp_info = args;//(aiaoic_args*)malloc(sizeof(aiaoic_args));
   // temp_info = args;
 
