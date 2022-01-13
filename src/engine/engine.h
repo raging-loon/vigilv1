ade27a8bb5de67cf98822448c1388303e14a42ec
raging-loon <cxmacolley@gmail.com>
2022-01-13 13:40:23 -0500

    modified

---
+++ src/engine/engine.h
@@ -0,0 +1,17 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+/*
+  *-*-*-*- engine.h -*-*-*-*
+  @purpose serve as the main place for suspicious traffic to be analyzed
+  TODO: Figure out if rule.c will be replaced with a function in here
+
+*/
+typedef struct{
+  
+} engine_data_t;
+
+void nshengine();
+
+
+#endif /* ENGINE_H */
