e8807b004a5ab18755ed1891f0775bb0ac5169e1
raging-loon <cxmacolley@gmail.com>
2022-02-03 11:19:28 -0500

    modified

--- src/filter/parsing/rule.c
+++ src/filter/parsing/rule.c
@@ -13,7 +13,8 @@
 void rulemgr(const struct rule_data * __rule_data){
   for(int i = 0; i < num_rules + 1;){
     struct rule * temp_rule = &rules[i++];
-    if((temp_rule->protocol == R_ALL) || ( __rule_data->__protocol == temp_rule->protocol)){
+    if(((temp_rule->protocol == R_ALL) || ( __rule_data->__protocol == temp_rule->protocol)) &&
+      ((temp_rule->port == -1) || (__rule_data->src_port == temp_rule->port || __rule_data->dest_port == temp_rule->port))){
       if(temp_rule->pkt_parser(__rule_data,temp_rule)){
         temp_rule->action(__rule_data,temp_rule,0);
         temp_rule->times_matched++;
