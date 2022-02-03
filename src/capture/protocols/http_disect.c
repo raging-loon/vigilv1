962c8753d9e65b25db4c953f6650964893c37c2d
raging-loon <cxmacolley@gmail.com>
2022-02-03 11:22:52 -0500

    modified

--- src/capture/protocols/http_disect.c
+++ src/capture/protocols/http_disect.c
@@ -6,6 +6,7 @@
 void http_disect(const unsigned char * pkt, const struct rule_data * rdata){
   // printf("%s\n",pkt);
   int loc = strloc(pkt,0x0d);
+  printf("Strloc successful\n");
   char request_hdr[255];
   strncpy(request_hdr,pkt,loc );
   printf("%s\n",request_hdr);
