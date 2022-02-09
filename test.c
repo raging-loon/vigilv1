195732a0e962675597d2c808afca1b318abbfcfc
raging-loon <cxmacolley@gmail.com>
2022-02-09 18:05:51 -0500

    modified

--- test.c
+++ test.c
@@ -3,13 +3,35 @@
 #include <string.h>
 
 int main(){
-  char * ipv6addr = "fe80:0000:0000:0000:0218:0aff:fe87:2ca5";
+  char ipv6addr[] = "fe80:0000:0000:0000:0218:0aff:fe87:2ca5";
   char shortaddr[strlen(ipv6addr)];
   char * hextet;
   hextet = strtok(ipv6addr,":");
+  int hextets_scanned = 0;
   while(hextet != NULL){
-    printf("%s\n",hextet);
+    int num_zeroes = 0;
     
-  }
+    for(int i = 0; i < strlen(hextet); i++){
+      if(hextet[i] == '0')
+        num_zeroes++;
+      else 
+        break;
+    }
 
+    if(num_zeroes == 4){
+      strcat(shortaddr,"0");
+    } else{
+      strncat(shortaddr,hextet,num_zeroes +1);
+    }
+
+
+    if(hextets_scanned != 7)
+      strcat(shortaddr,":");
+    else
+      break;
+    
+    hextets_scanned++;
+    hextet = strtok(NULL,":");
+  }
+  printf("%s\n",shortaddr);
 }
