abfadbdc4de57fc7c9a4fbd1f2f3808240249982
raging-loon <cxmacolley@gmail.com>
2022-02-15 09:35:55 -0500

    modified:   src/filter/parsing/line_parser.c 	modified:   src/filter/parsing/rule.c

--- src/filter/parsing/line_parser.c
+++ src/filter/parsing/line_parser.c
@@ -87,7 +87,9 @@ static void sc_strip(char * sub){
 
 
 static void void_rule(struct rule * r){
-  // memset(r,0,sizeof(r));
+  memset(&r->icmp_data, 0,sizeof(r->icmp_data));
+  memset(&r->ip_data,   0,sizeof(r->ip_data));
+  memset(&r->tcp_data,  0,sizeof(r->tcp_data));
 }
 
 
