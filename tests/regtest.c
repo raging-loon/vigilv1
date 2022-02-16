ca59d0107b66a508019e18a2c48c81e62b1684ed
raging-loon <cxmacolley@gmail.com>
2022-02-16 18:18:55 -0500

    modified

--- tests/regtest.c
+++ tests/regtest.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
-struct data{
-  unsigned int data1 : 16;
-  unsigned int data2 : 16;
-  unsigned int data3 : 32;
-};
+#include <string.h>
+char packet[] = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00\x45\x00" \
+"\x00\x54\x7f\x8e\x40\x00\x40\x01\xbd\x18\x7f\x00\x00\x01\x7f\x00" \
+"\x00\x01\x08\x00\xc2\x45\x00\x02\x00\x01\x53\x84\x0d\x62\x00\x00" \
+"\x00\x00\x08\xfe\x0d\x00\x00\x00\x00\x00\x10\x11\x12\x13\x14\x15" \
+"\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25" \
+"\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35" \
+"\x36\x37";
 
 int main(){
-  struct data * d = (struct data *)malloc(sizeof(struct data));
-  d->data1 = 0x6261;
-  d->data2 = 0x6463;
-  d->data3 = 0x90676665;
-  printf("%s\n",d);
-
+  int checksum;
   
+  for (int i = 0; i < strlen(packet) - 1; i += 2)
+    checksum += (packet[i] << 8) + packet[i + 1];
+  if(sizeof(packet) % 2)
+    checksum += packet[sizeof(packet) - 1];
+  printf("%d\n",checksum);
 }
