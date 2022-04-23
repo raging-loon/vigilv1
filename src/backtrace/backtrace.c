d5b3abc1afe84f198387133914aaff1eb11813ee
raging-loon <cxmacolley@gmail.com>
2022-04-23 12:26:24 -0400

    mod

--- src/backtrace/backtrace.c
+++ src/backtrace/backtrace.c
@@ -2,8 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <execinfo.h>
+#include "../utils.h"
 
-
-void load_fn_io_map(){
+void crash_handler(int sig){
+  register unsigned int const r_rax __asm__("rax");
+  register unsigned int const r_rbx __asm__("rbx");
+  register unsigned int const r_rcx __asm__("rcx");
+  register unsigned int const r_rdx __asm__("rdx");
+  register unsigned int const r_rsp __asm__("rsp");
+  register unsigned int const r_rbp __asm__("rbp");
   
+  void * array[20];
+  char ** strings;
+  int size = backtrace(array, 20);
+  strings = backtrace_symbols(array,size);
+  printf("Segmentation fault at %s: application crashed\n",get_formated_time());
+  printf("Function backtrace:\n");
+  if(strings != NULL){
+    for(int i = 0; i < size; i++){
+      printf("%s\n",strings[i]);
+    }
+  }
+  free(strings);
+  
+  printf("Register Dump:\n");
+  printf("[RAX] = 0x%02x\n",r_rax);
+  printf("[RBX] = 0x%02x\n",r_rbx);
+  printf("[RCX] = 0x%02x\n",r_rbx);
+  printf("[RDX] = 0x%02x\n",r_rcx);
+  printf("[RSP] = 0x%02x\n",r_rsp);
+  printf("[RBP] = 0x%02x\n",r_rbp);
+  exit(EXIT_FAILURE); 
+
 }
--- src/backtrace/backtrace.h
+++ src/backtrace/backtrace.h
@@ -1,10 +1,15 @@
 #ifndef BACKTRACE_H
 #define BACKTRACE_H
-
+/*
 typedef struct{
   char fn_name[32];
   unsigned int mem_loc;
 } fn_io_map;
 
 void load_fn_io_map();
+*/
+
+void crash_handler(int );
+
+
 #endif /* BACKTRACE_H */
