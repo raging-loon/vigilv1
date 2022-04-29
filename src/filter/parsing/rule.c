a9a92bc67b1e5cc394b70ada4724a0a28177415b
raging-loon <cxmacolley@gmail.com>
2022-04-29 09:50:16 -0400

    modified

--- src/filter/parsing/rule.c
+++ src/filter/parsing/rule.c
@@ -48,8 +48,7 @@ void rulemgr(const struct rule_data * __rule_data){
         if(vigil_location == INTERNAL){
           if((temp_rule->src_port == R_ALL || (temp_rule->src_port == __rule_data->src_port)) &&
                   (temp_rule->dest_port == R_ALL || (temp_rule->dest_port == __rule_data->dest_port))){
-                    // if(test_tcp_session_status(temp_rule,__rule_data) && 
-                    if(test_encrypt(temp_rule,__rule_data))
+                    if(test_tcp_session_status(temp_rule,__rule_data) && test_encrypt(temp_rule,__rule_data))
                       rule_app(temp_rule,__rule_data);
           }
         } 
@@ -70,8 +69,7 @@ void rulemgr(const struct rule_data * __rule_data){
           else if((temp_rule->src_port == R_ALL || (temp_rule->src_port == __rule_data->src_port)) &&
                   (temp_rule->dest_port == R_ALL || (temp_rule->dest_port == __rule_data->dest_port))){
                     // if(test_tcp_session_status(temp_rule,__rule_data))
-                    // if(test_tcp_session_status(temp_rule,__rule_data) &&
-                     if(test_encrypt(temp_rule,__rule_data))
+                    if(test_tcp_session_status(temp_rule,__rule_data) && test_encrypt(temp_rule,__rule_data))
                       rule_app(temp_rule,__rule_data);
                     
                   }
