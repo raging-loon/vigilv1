31c72e8243a283a1901af1e52e42005e6da0ce15
raging-loon <cxmacolley@gmail.com>
2022-04-29 09:17:13 -0400

    modified

--- src/capture/tls.c
+++ src/capture/tls.c
@@ -2,12 +2,29 @@
 #include "tls.h"
 #include "../filter/parsing/rule.h"
 #include <pcap.h>
+#include "../../globals.h"
 
 void tls_decode(const unsigned char * pkt ,struct rule_data * rdata,const struct pcap_pkthdr * pkt_hdr){
   struct tlsd * __tlsdata = (struct tlsd *)(pkt);
+
+  switch(__tlsdata->version){
+    case 0x0301:
+      if(packet_print) printf("TLS 1.0 DATA: ");    
+      break;
+    case 0x0302:
+      if(packet_print) printf("TLS 1.2 DATA: ");
+      break;
+    case 0x0303:
+      if(packet_print) printf("TLS 1.3 DATA: ");
+      break;
+  }
+
   switch(__tlsdata->content_type){
     case 0x14:
       // change cipher spec
+      break;
+    case 0x15:
+      // alert
       break;
     case 0x16:
       tls_handshake(pkt,rdata,pkt_hdr);
@@ -23,5 +40,29 @@ void tls_decode(const unsigned char * pkt ,struct rule_data * rdata,const struct
 
 void tlshandshake(const unsigned char * pkt,struct rule_data * rdata,const struct pcap_pkthdr * pkt_hdr){
   struct tls_handshake * tlsh = (struct tls_handshake *)(pkt + sizeof(struct tlsd));
-  
+  switch(tlsh->handshake_type){
+    case 0x01:
+      if(packet_print) printf("Client Hello\n");
+      rdata->encrypted = true;
+      break;
+    case 0x02:
+      if(packet_print) printf("Server Hello\n");
+      rdata->encrypted = true;
+      break;
+    case 0x04:
+      if(packet_print) printf("New Session Ticket\n");
+      rdata->encrypted = true;
+      break;    
+    case 0x0b:
+      if(packet_print) printf("Certificate\n");
+      rdata->encrypted = true;
+      break;
+
+    case 10:
+      if(packet_print) printf("Client Kex\n");
+      rdata->encrypted = true;
+      break;
+    
+    
+  }
 }
