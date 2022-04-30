57bd5920c55fcb15bfda1e8b9431ce6bb66b2590
raging-loon <cxmacolley@gmail.com>
2022-04-29 20:17:22 -0400

    mod

--- src/capture/pktmgr.c
+++ src/capture/pktmgr.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2021-2022 Conner Macolley
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
 #include <pcap.h>
 #include <netinet/ether.h>
 #include "pktmgr.h"
@@ -10,31 +27,7 @@
 #include "l3pktmgr.h"
 #include "../statistics/wclean.h"
 #include "ip6decode.h"
-/*!
-  @file
-  @author Conner Macolley <cxmacolley@gmail.com>
 
-  @section LICENSE
-
- Copyright 2021 Conner Macolley
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
- 
-  @section DESCRIPTION
-
-  Disect the ethernet header and call functions based on that.
-  
-*/
 void pktmgr(unsigned char *user, const struct pcap_pkthdr *pkt_hdr, const unsigned char *pkt){
   
   if(packet_print) printf("\033[90m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\033[m\n");
