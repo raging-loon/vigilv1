984acd552d1aa460c63611e88a81d67feb02a530
raging-loon <cxmacolley@gmail.com>
2022-05-10 11:46:36 -0400

    added vkex

--- src/vrmc/handle_client.c
+++ src/vrmc/handle_client.c
@@ -2,10 +2,15 @@
 #include "opcodes.h"
 #include <stdbool.h>
 #include <sys/socket.h>
+// #include <base64.h>
 #include "../../globals.h"
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/types.h>
+static __thread char buffer[1024];
+
+#define CLEAR_BUFFER memset(&buffer,0,sizeof(buffer));
+
 
 void handle_client(void * cptr){
   connect_t * cnx = (connect_t*)cptr;
@@ -14,16 +19,22 @@ void handle_client(void * cptr){
   while(true){
     if(stage == STAGE_VERSION){
       version_exchange(cnx);
+      stage = STAGE_KEX;
+    } 
+    else if(stage == STAGE_KEX){
+      vkex(cnx);
+      stage = STAGE_KEX;
     }
+
   }
   close(cnx->fd);
 }
 
 
 void version_exchange(connect_t * cnx){
+  CLEAR_BUFFER
   send(cnx->fd,VERSION,strlen(VERSION),0);
 
-  char buffer[1024];
   int len_read = read(cnx->fd,buffer,1024);
   if(len_read <= 0){
     close(cnx->fd);
@@ -31,4 +42,18 @@ void version_exchange(connect_t * cnx){
 
     return;
   }
+}
+
+void vkex(connect_t * cnx){
+  CLEAR_BUFFER
+  send(cnx->fd,pubkey,pubkey_len,0);
+  int len_read = read(cnx->fd,buffer,1024);
+  if(len_read <= 0){
+    close(cnx->fd);
+  } else if(!(strncmp(pubkey,buffer,len_read) == 0)){
+    close(cnx->fd);
+  } else {
+    return;
+  }
+
 }
