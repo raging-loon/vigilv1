2665c9d40e4c026676e885955dc443d3c554e21b
raging-loon <cxmacolley@gmail.com>
2022-05-10 16:09:42 -0400

    started custom pcap lib

--- src/capture/capture.h
+++ src/capture/capture.h
@@ -1,10 +1,12 @@
 #ifndef CAPTURE_H
 #define CAPTURE_H
-
+#include <sys/socket.h>
+#include <netinet/in.h>
 struct vpcap{
   // int mode;
   char * interface;
   int fd;
+  struct sockaddr_in sock;
 };
 
 void init_pcap_int(struct vpcap *, char *);
