d71a54738ade2f9e29fcf2e16b2f5567e45ef54e
raging-loon <cxmacolley@gmail.com>
2022-05-14 14:07:56 -0400

    started working on netif

--- src/netif/netif.c
+++ src/netif/netif.c
@@ -8,21 +8,34 @@ v_netif * net_interfaces;
 
 
 void detect_interfaces(){
-  net_interfaces = (v_netif *)malloc(sizeof(v_netif));
-  
-
-
   struct ifaddrs * ifaces;
   if(getifaddrs(&ifaces) == -1){
     perror("Failed in getifaddrs in netif.c");
     return;
   }
+  net_interfaces = (v_netif *)malloc(sizeof(v_netif));
+  int iface_detected = 0;
 
+
+  struct ifaddrs * n_addr_ptr = ifaces;
   while(ifaces){
-    int addr_family = ifaces->ifa_addr->sa_family;
+    int addr_family = n_addr_ptr->ifa_addr->sa_family;
 
     if(addr_family == AF_INET || addr_family == AF_INET6){
-      
+      if(iface_detected != 0){
+        net_interfaces = (v_netif *)realloc(net_interfaces,sizeof(net_interfaces) * iface_detected);
+      }
+      // v_netif * current_iface = net_interfaces[iface_detected];
+
+
+      iface_detected++;
     }
+    n_addr_ptr = n_addr_ptr->ifa_next;
+    
   }
+  freeifaddrs(ifaces);
+}
+
+void free_iface(){
+  free(net_interfaces);
 }
