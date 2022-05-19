7febe0ebcfff25a5a4c40addbb2aec37e15f1dc0
raging-loon <cxmacolley@gmail.com>
2022-05-19 09:42:44 -0400

    finished interface detection

--- src/capture/capture.h
+++ src/capture/capture.h
@@ -2,11 +2,14 @@
 #define CAPTURE_H
 #include <sys/socket.h>
 #include <netinet/in.h>
+
 struct vpcap{
   // int mode;
   char * interface;
   int fd;
   struct sockaddr_in sock;
+  int mode;
+  int tid;
 };
 
 void init_pcap_int(struct vpcap *, char *);
