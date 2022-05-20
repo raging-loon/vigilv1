240d7eabf9ebd8acd31cbeca88e982313935b798
raging-loon <cxmacolley@gmail.com>
2022-05-20 10:19:37 -0400

    started adding more rule actions

--- ipcmon/main.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
---
+++ ipcrouter/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <getopt.h>
+
+int main(int argc, char ** argv){
+  
+}
