f36d763b46bb02ed12006c270ffba9ad1be6f29f
raging-loon <cxmacolley@gmail.com>
2022-05-22 12:27:19 -0400

    mod

--- ipcrouter/main.c
+++ ipcrouter/main.c
@@ -9,21 +9,16 @@ typedef struct ipc_queue_mem {
 	char mesg_text[256];
 } ipc_queue_mem;
 
-int main()
-{
+int main(){
 	key_t key;
 	int msgid;
-
 	key = ftok("progfile", 65);
-
 	msgid = msgget(key, 0666 | IPC_CREAT);
 	while(1){
 		ipc_queue_mem message;
 		msgrcv(msgid, &message, sizeof(message), 1, 0);
-
 		printf("%s", message.mesg_text);
 	}
-
 	msgctl(msgid, IPC_RMID, NULL);
 
 	return 0;
