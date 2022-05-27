7ab6bc1c9ed6ade6d8fe68d415e02e3c63756985
raging-loon <cxmacolley@gmail.com>
2022-05-27 12:00:51 -0400

    working on raw socket

--- tests/test.c
+++ tests/test.c
@@ -1,24 +1,33 @@
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
 #include <stdio.h>
-#include <dirent.h>
+#include <stdlib.h>
 
-int main(void)
+int main()
 {
-	struct dirent *de; // Pointer for directory entry
+struct ifaddrs *addresses;
+if (getifaddrs(&addresses) == -1)
+{
+printf("getifaddrs call failed\n");
+return -1;
+}
 
-	// opendir() returns a pointer of DIR type.
-	DIR *dr = opendir(".");
-
-	if (dr == NULL) // opendir returns NULL if couldn't open directory
-	{
-		printf("Could not open current directory" );
-		return 0;
-	}
-
-	// Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
-	// for readdir()
-	while ((de = readdir(dr)) != NULL)
-			printf("%s\n", de->d_name);
-
-	closedir(dr);	
-	return 0;
+struct ifaddrs *address = addresses;
+while(address)
+{
+int family = address->ifa_addr->sa_family;
+if (family == AF_INET || family == AF_INET6)
+{
+printf("%s\t", address->ifa_name);
+printf("%s\t", family == AF_INET ? "IPv4" : "IPv6");
+char ap[100];
+const int family_size = family == AF_INET ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+getnameinfo(address->ifa_addr,family_size, ap, sizeof(ap), 0, 0, NI_NUMERICHOST);
+printf("\t%s\n", ap);
+}
+address = address->ifa_next;
+}
+freeifaddrs(addresses);
+return 0;
 }
