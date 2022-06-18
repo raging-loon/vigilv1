a1abe6f872f95a4698412eaa55ba7806bbabfc37
raging-loon <cxmacolley@gmail.com>
2022-06-18 12:09:37 -0400

    added to spi

--- vigil/engine/uptcp.c
+++ vigil/engine/uptcp.c
@@ -10,9 +10,9 @@ void tcp_syn_handler(struct spi_members * sm){
   if(sm->status == __SPI_UNINIT)
     sm->status = __TCP_INIT;
   
-  // printf("SPI loc:%d New TCP Conversation: 1/3 3WH %s:%d -> %s:%d\n",
-          // sm->location, sm->cli_addr,sm->cli_port,sm->serv_addr,sm->serv_port);
-  // fflush(stdout);
+  printf("SPI loc:%d New TCP Conversation: 1/3 3WH %s:%d -> %s:%d\n",
+          sm->location, sm->cli_addr,sm->cli_port,sm->serv_addr,sm->serv_port);
+  fflush(stdout);
 }
 
 void tcp_syn_ack_handler(struct spi_members * sm){
@@ -22,17 +22,18 @@ void tcp_syn_ack_handler(struct spi_members * sm){
   
   if(sm->status == __TCP_INIT){
     sm->status = __TCP_ACK_W;
-    //  printf("SPI TCP Conversation: 2/3 3WH %s:%d -> %s:%d\n",
-          // sm->cli_addr,sm->cli_port,sm->serv_addr,sm->serv_port);
-    // fflush(stdout); 
+     printf("SPI TCP Conversation: 2/3 3WH %s:%d -> %s:%d\n",
+          sm->cli_addr,sm->cli_port,sm->serv_addr,sm->serv_port);
+    fflush(stdout); 
   }
 }
 void tcp_ack_handler(struct spi_members * sm){
   if(sm == NULL) return;
 
   if(sm->status == __TCP_ACK_W){
-    // printf("SPI TCP Conversation 3WH complete: %s:%d -> %s:%d\n", 
-    // sm->cli_addr, sm->cli_port, sm->serv_addr, sm->serv_port);
+    printf("SPI TCP Conversation 3WH complete: %s:%d -> %s:%d\n", 
+    sm->cli_addr, sm->cli_port, sm->serv_addr, sm->serv_port);
+    fflush(stdout);
     sm->status = __TCP_ESTABLISHED;
   }
 }
@@ -45,6 +46,7 @@ void tcp_rst_handler(struct spi_members * sm){
   }
   if(sm->status == __TCP_ACK_W){
     printf("Possible syn scan detected %s:%d -> %s:%d",sm->cli_addr,sm->cli_port,sm->serv_addr, sm->serv_port);
+    fflush(stdout);
     sm->conversation_active = false;
   }
 }
