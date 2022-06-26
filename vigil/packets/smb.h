5f1c5f2f9aa450ea727d19df7e840673792a6a98
raging-loon <cxmacolley@gmail.com>
2022-06-26 11:42:37 -0400

    round 4 of adding licesnes

--- vigil/packets/smb.h
+++ vigil/packets/smb.h
@@ -1,6 +1,21 @@
+/*     
+    This file is part of Vigil.
+
+    Vigil is free software: you can redistribute it and/or modify it under the terms of the 
+    GNU General Public License as published by the Free Software Foundation, either version 3 
+    of the License, or (at your option) any later version.
+
+    Vigil is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with Vigil. 
+    If not, see <https://www.gnu.org/licenses/>. 
+
+*/
 #ifndef __SMB_H
 #define __SMB_H
-
+#include <stdint.h>
 struct smb2{
   uint32_t proto_id;
   uint16_t hdr_ln;
