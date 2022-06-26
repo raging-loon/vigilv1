6a44fc5766f0cd9f7c66f5aff92beb36c0f9e858
raging-loon <cxmacolley@gmail.com>
2022-06-26 11:46:09 -0400

    round 5 of adding licesnes

--- vigil/main.c
+++ vigil/main.c
@@ -1,33 +1,28 @@
-/*!
- * @file main.c
- * @author Conner Macolley
- * @brief Start everything and start services
- * @section LICENSE
- * Copyright 2021-2022 Conner Macolley
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- * @section DESCRIPTION
- * The main function handles all of the following:
- *  - Loading the function map
- *  - Set signals for interrupts and crashes
- *  - Initialize the throughout monitor
- *  - Get CPU info(for cosmetics)
- *  - Gather ip addresses from the blacklist
- *  - Parse and store all of the rules
- *  - Initialize all of the rules
- *  - Collect Lua Scripts
- *  - Start the VRMC server
- *  - Start the capture process
+/*     
+    This file is part of Vigil.
+
+    Vigil is free software: you can redistribute it and/or modify it under the terms of the 
+    GNU General Public License as published by the Free Software Foundation, either version 3 
+    of the License, or (at your option) any later version.
+
+    Vigil is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with Vigil. 
+    If not, see <https://www.gnu.org/licenses/>. 
+
+  The main function handles all of the following:
+   - Loading the function map
+   - Set signals for interrupts and crashes
+   - Initialize the throughout monitor
+   - Get CPU info(for cosmetics)
+   - Gather ip addresses from the blacklist
+   - Parse and store all of the rules
+   - Initialize all of the rules
+   - Collect Lua Scripts
+   - Start the VRMC server
+   - Start the capture process
  */
 #include <stdio.h>
 
