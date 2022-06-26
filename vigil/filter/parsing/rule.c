17ae34ab66d4e1b4a352c5881b767ab1077c6063
raging-loon <cxmacolley@gmail.com>
2022-06-26 11:38:05 -0400

    round 2 of adding licesnes

--- vigil/filter/parsing/rule.c
+++ vigil/filter/parsing/rule.c
@@ -5,17 +5,14 @@
     GNU General Public License as published by the Free Software Foundation, either version 3 
     of the License, or (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    Vigil is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
     without  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
     See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with Foobar. 
+    You should have received a copy of the GNU General Public License along with Vigil. 
     If not, see <https://www.gnu.org/licenses/>. 
 
 */
-
-
-
 
 #include "rule.h"
 #include "../../globals.h"
